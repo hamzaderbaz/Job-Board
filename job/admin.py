fdf387aa2e0c5f14448bb4d121e4279868f19b99
Your Name <you@example.com>
2022-06-02 02:59:56 +0200

    add job models

--- job/admin.py
+++ job/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Job   # عملنا  . علشان همل الاتنين في نفس الفولدر 
+
+admin.site.register(Job) #  
--- job/models.py
+++ job/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+
+
+''' 
+django model field :
+-html widget
+-validation 
+-db size
+
+'''
+
+JOB_TYPE = (
+
+    ('Full Time','Full Time'),
+    ('Part Time ','Part Time'),
+)
+
+
+
+class Job(models.Model):      #class equal thing in DB is table   # انشأنا  جدول في الداتابيز اسمه Job  
+    title = models.CharField(max_length = 100)     #column   # وانشأنا تحته عمود اسمه titl  
+    #location
+    job_type = models.CharField(max_length = 15, choices= JOB_TYPE)
+    description = models.TextField(max_length = 1000 )
+    published_at = models.DateTimeField(auto_now = False)
+    vacancy = models.IntegerField(default=1)
+    salary = models.IntegerField(default=0)
+    experience = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.title
+
+
+
+
+

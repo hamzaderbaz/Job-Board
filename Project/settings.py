300e98dd1cbd152401be14713a4f834cc1ef4871
hamzaderbaz <hamza.derbaz@gmail.com>
2023-11-24 22:00:02 +0200

    edit project

--- Project/settings.py
+++ Project/settings.py
@@ -98,7 +98,7 @@ TEMPLATES = [
 
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
 
-        'DIRS': ['templates'],
+        'DIRS': [BASE_DIR, 'templates'],
 
         'APP_DIRS': True,
 
